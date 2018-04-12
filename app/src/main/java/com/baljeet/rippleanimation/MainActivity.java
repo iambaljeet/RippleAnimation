d0c9c4cb00fc09bf327671d3c5ce5c02c1647594
iambaljeet <ibaljeet@outlook.com>
2018-04-13 00:21:14 +0530

    Added Ripple Animation

--- app/src/main/java/com/baljeet/rippleanimation/MainActivity.java
+++ app/src/main/java/com/baljeet/rippleanimation/MainActivity.java
@@ -2,12 +2,28 @@ package com.baljeet.rippleanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.baljeet.rippleanimation.view.RippleBackground;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RippleBackground rippleBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rippleBackground = findViewById(R.id.content);
+
+        rippleBackground.startRippleAnimation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        rippleBackground.stopRippleAnimation();
     }
 }
